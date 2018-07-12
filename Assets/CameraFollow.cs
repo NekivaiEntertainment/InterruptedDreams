c78e4fe430c389829c20f8c41efd3ff5b58ac4ba
Michael <panmihaurkin@icloud.com>
2018-07-12 20:09:53 +0700

    Basic movement and art style

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{    
+    public Transform target;
+
+    public float smoothTime = 0.125f;
+    public Vector3 offset;
+
+    private Vector3 desPos;
+    private Vector3 smoothPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate ()
+    {
+        desPos = target.position + offset;
+        smoothPos = Vector3.SmoothDamp(transform.position, desPos, ref velocity, smoothTime);
+        transform.position = smoothPos;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour 
+{
+	CharacterController charControl;
+	public float walkSpeed = 4f;
+
+	void Awake () 
+	{
+		charControl = GetComponent<CharacterController>();
+	}
+
+	void Update () 
+	{
+		MovePlayer();
+	}
+
+	void MovePlayer()
+	{
+		float horiz = Input.GetAxis("Horizontal");
+		float vert = Input.GetAxis("Vertical");
+
+		Vector3 moveDirectionalS = transform.right * horiz * walkSpeed;
+		Vector3 moveDirectionalF = transform.forward * vert * walkSpeed;
+
+		charControl.SimpleMove(moveDirectionalS);
+		charControl.SimpleMove(moveDirectionalF);
+	}
+}
