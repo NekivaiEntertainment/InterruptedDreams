24f936788b7d219ea84fa68c74324d38aefd0137
Michael <panmihaurkin@icloud.com>
2018-07-16 22:31:57 +0700

    Added book props, new interactives and fixed collision

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{    
+    public Transform target;
+
+    public float smoothTime = 0.125f;
+    public Vector3 offsetPosition;
+    public Vector3 offsetTarget;
+
+    private Vector3 desPos;
+    private Vector3 smoothPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate ()
+    {
+        desPos = target.position + offsetPosition;
+        smoothPos = Vector3.SmoothDamp(transform.position, desPos, ref velocity, smoothTime);
+        transform.position = smoothPos;
+
+        transform.LookAt(target.position + offsetTarget);
+    }
+}
