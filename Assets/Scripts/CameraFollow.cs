561e5882d22460682aba8ddfbd789c97b2370f12
Michael <panmihaurkin@icloud.com>
2018-07-26 21:32:24 +0700

    Added Room Sections

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,6 +8,10 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offsetPosition;
     public Vector3 offsetTarget;
 
+    [Header("Stopping distance")]
+    public float closeStop = 0;
+    public float farStop = 0;
+
     private Vector3 desPos;
     private Vector3 smoothPos;
     private Vector3 velocity = Vector3.zero;
@@ -21,7 +25,9 @@ public class CameraFollow : MonoBehaviour
     {
         desPos = target.position + offsetPosition;
         smoothPos = Vector3.SmoothDamp(transform.position, desPos, ref velocity, smoothTime);
+        smoothPos.z = Mathf.Clamp(smoothPos.z, closeStop, farStop);
         transform.position = smoothPos;
+
 
         transform.LookAt(target.position + offsetTarget);
     }
