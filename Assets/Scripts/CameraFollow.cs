17a64383ba5703fa18889691bab0ece373f9c4b4
Michael <panmihaurkin@icloud.com>
2018-08-09 19:38:12 +0700

    Camera alpha

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,40 +2,17 @@
 using EZCameraShake;
 
 public class CameraFollow : MonoBehaviour
-{    
-    public Transform target;
-
-    public float smoothTime = 0.125f;
-    public Vector3 offsetPosition;
-    public Vector3 offsetTarget;
-
-    [Header("Stopping Distance")]
-    public float closeStop = 0;
-    public float farStop = 0;
-
+{
     [Header("Shake Controls")]
     public float magnitude = 0.5f;
     public float roughness = 0.5f;
     public float fadeInTime = 5f;
 
-    private Vector3 desPos;
-    private Vector3 smoothPos;
-    private Vector3 velocity = Vector3.zero;
-
-    void Awake()
-    {
-        transform.position = target.position + offsetPosition + new Vector3(0f, 0f, -2f);
-    }
+    [Header("Player")]
+    public Transform target;
 
     void LateUpdate ()
     {
-        desPos = target.position + offsetPosition;
-        smoothPos = Vector3.SmoothDamp(transform.position, desPos, ref velocity, smoothTime);
-        smoothPos.z = Mathf.Clamp(smoothPos.z, closeStop, farStop);
-        transform.position = smoothPos;
-
-        transform.LookAt(target.position + offsetTarget);
-
         CameraShaker.Instance.StartShake(magnitude, roughness, fadeInTime);
     }
 }
